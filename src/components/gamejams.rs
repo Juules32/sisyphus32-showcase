5f0f1ebc0c6d372243eaff1de1a5c92438a16cf6
juules32 <56916699+Juules32@users.noreply.github.com>
2025-05-24 17:51:58 +0200

    included itch widgets and dynamic styling

--- src/components/gamejams.rs
+++ src/components/gamejams.rs
@@ -1,13 +1,50 @@
 use dioxus::prelude::*;
 
-use crate::components::{Layout, Lorem};
+use crate::components::Layout;
 
 #[component]
 pub fn GameJams() -> Element {
     rsx! {
         Layout {
-            h1 { "Game Jams!" }
-            Lorem {}
+            h1 { "Game Jams" }
+            p {
+                "
+                    Over the years, I have participated in a few game jams which are listed below.
+                    Keep in mind all of these were made from start to finish in the span of a few
+                    days, so you might encounter a few bugs...
+                "
+            }
+            div {
+                id: "gamejam-entry",
+                iframe {
+                    id: "itch-widget",
+                    frame_border: "0",
+                    src: "https://itch.io/embed/3451248",
+                    a {
+                        href: "https://https://juules32.itch.io/potato-protecc",
+                        "Potato Protecc by juules32"
+                    }
+                }
+                a {
+                    "Template game jam text..."
+                }
+            }
+
+            div {
+                id: "gamejam-entry",
+                iframe {
+                    id: "itch-widget",
+                    frame_border: "0",
+                    src: "https://itch.io/embed/2659991",
+                    a {
+                        href: "https://https://juules32.itch.io/braininvader",
+                        "BrainInvader by juules32"
+                    }
+                }
+                a {
+                    "Template game jam text..."
+                }
+            }
         }
     }
 }
--- src/components/webapps.rs
+++ src/components/webapps.rs
@@ -6,7 +6,43 @@ use crate::components::Layout;
 pub fn WebApps() -> Element {
     rsx! {
         Layout {
-            iframe { id: "pokelink", src: "https://pokelink.juules32.com/" }
+            h1 { "Web Apps" }
+            p {
+                "
+                    In my free time, I like to explore new tools and technologies to
+                    build hobby pages.
+                "
+            }
+            
+            h2 { "PokéLink" }
+
+            div {
+                id: "pokelink",
+                p {
+                    "
+                        PokéLink is a Pokémon-inspired browser game in the \"dle\" genre
+                        (wordle, globle, etc.) 
+                    "
+                }
+                p {
+                    id: "playbelow",
+                    "You can play the game below or visit the website "
+                    Link {
+                        to: "https://pokelink.juules32.com/",
+                        "here!"
+                    }
+                }
+                p {
+                    id: "playlink",
+                    "You can play the game "
+                    Link {
+                        to: "https://pokelink.juules32.com/",
+                        "here!"
+                    }
+                }
+                iframe { src: "https://pokelink.juules32.com/", allowfullscreen: "" }
+            }
+
         }
     }
 }
